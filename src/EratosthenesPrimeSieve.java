cf6bcacd3af52c0dddb241250a4d6cb29c5f7344
koglerm190074 <koglerm190074@sus.htl-grieskirchen.at>
2021-09-21 09:41:05 +0200

    fourth commit

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -35,7 +35,63 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
 
     @Override
     public void printPrimes() {
+        boolean[] arr = new boolean[obergrenze];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=true;
 
+        }
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                if((i*j)>=arr.length){
+                    break;
+                }
+                arr[i*j] = false;
+            }
+
+        }
+        for (int i = 2; i < arr.length; i++) {
+            if(arr[i]==true){
+                System.out.println(i);
+            }
+        }
+    }
+
+    public void primeAdd(){
+        boolean[] arr = new boolean[obergrenze];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=true;
+
+        }
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                if((i*j)>=arr.length){
+                    break;
+                }
+                arr[i*j] = false;
+            }
+
+        }
+        /*for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                if(arr[i]==true&&arr[j]==true){
+                    if((i+j)==n){
+                        System.out.println(n+" summe: "+n+" = "+i+" + "+j);
+                    }
+                }
+            }
+        }*/
+
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                if((i%2)==0){
+                    if(arr[i]==true&&arr[j]==true){
+                        if((i+j)==i){
+                            System.out.println(i+" summe: "+i+" = "+i+" + "+j);
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean running = true;
+        while(running){
+            System.out.println("Geben Sie eine Obergrenze ein!!!");
+            int obergrenze = Integer.parseInt(s.nextLine());
+            System.out.println("Geben Sie eine zu überprüfende Zahl ein!!!");
+            int prime = Integer.parseInt(s.nextLine());
+            EratosthenesPrimeSieve eps = new EratosthenesPrimeSieve(obergrenze);
+            if(eps.isPrime(prime)==false){
+                System.out.println("Keine Primzahl");
+            }else if(eps.isPrime(prime)==true){
+                System.out.println("Ist eine Primzahl");
+            }
+            eps.printPrimes();
+            System.out.println("Geben Sie eine Gerade Zahl über 1 ein!");
+            int n = Integer.parseInt(s.nextLine());
+            eps.primeAdd(n);
+            System.out.println("Noch einmal?(j/n)");
+            String answer = s.nextLine();
+            if(answer.equals("n")){
+                System.out.println("End!");
+                running = false;
+            }
+        }
 
     }
 }
