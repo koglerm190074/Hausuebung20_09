f263231f80992f7333cdf2d2950e11ce631a392c
koglerm190074 <koglerm190074@sus.htl-grieskirchen.at>
2021-09-21 09:31:42 +0200

    third commit

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,11 +1,41 @@
 public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private int obergrenze;
+
+    public EratosthenesPrimeSieve(int obergrenze) {
+        this.obergrenze = obergrenze;
+    }
+
     @Override
-    public boolean isPrime(int p) {
-        return false;
+    public boolean isPrime(int p) {//funktioniert ausser obergrenze = p
+
+        boolean[] arr = new boolean[obergrenze];
+        boolean prime = false;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=true;
+
+        }
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = 2; j < arr.length; j++) {
+                if((i*j)>=arr.length){
+                    break;
+                }
+                arr[i*j] = false;
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==true&&i==p){
+                prime = true;
+            }
+        }
+
+        return prime;
     }
 
     @Override
     public void printPrimes() {
 
     }
+
 }
